c2f3c0f60b12c4f8cab0b44e54a2f94747004c2b
Ciprian Ceteras <cipix2000@gmail.com>
2016-12-12 12:44:15 +0200

    better docs for flatdata plugin

--- jquery.flot.flatdata.js
+++ jquery.flot.flatdata.js
@@ -1,11 +1,25 @@
-/* Support flat 1D data.
+/* Support for flat 1D data series.
 
-Set series.flatdata to true to enable.
+A 1D flat data series is a data series in the form of a regular 1D array. The
+main reason for using a flat data series is that it performs better, consumes
+less memory and generates less garbage collection than the regular flot format.
+
+Example:
+
+    plot.setData([[[0,0], [1,1], [2,2], [3,3]]]); // regular flot format
+    plot.setData([{flatdata: true, data: [0, 1, 2, 3]}]); // flatdata format
+
+Set series.flatdata to true to enable this plugin.
+
+You can use series.start to specify the starting index of the series (default is 0)
+You can use series.step to specify the interval between consecutive indexes of the series (default is 1)
 */
 
 /* global jQuery*/
 
 (function ($) {
+    'use strict';
+
     function process1DRawData(plot, series, data, datapoints) {
         if (series.flatdata === true) {
             var start = series.start || 0;
@@ -24,6 +38,6 @@ Set series.flatdata to true to enable.
             plot.hooks.processRawData.push(process1DRawData);
         },
         name: 'flatdata',
-        version: '0.0.1'
+        version: '0.0.2'
     });
 })(jQuery);
