471722f60df1c64174f843c0480a1fe340650759
Raluca Portase <raluca.portase@ni.com>
2017-03-22 16:48:58 +0200

    Precision check for negative values added

--- tests/jquery.flot-precision.Test.js
+++ tests/jquery.flot-precision.Test.js
@@ -3,7 +3,7 @@ describe("unit tests for the precision of axis", function() {
     var plot;
     var placeholder;
     var sampledata = [[0, 1], [1, 1.1], [2, 1.2]];
-    
+
     beforeEach(function () {
         var fixture = setFixtures('<div id="demo-container" style="width: 800px;height: 600px">').find('#demo-container').get(0);
 
@@ -17,10 +17,10 @@ describe("unit tests for the precision of axis", function() {
         }
         $('#placeholder').empty();
     });
-    
+
     it('should use the precision given by tickDecimals when specified', function() {
         var options = [];
-        
+
         plot = $.plot("#placeholder", [sampledata], {});
 
         var testVector = [
@@ -30,7 +30,7 @@ describe("unit tests for the precision of axis", function() {
             [1, 1.1, 5, 1, 1],
             [1, 1.00000000000001, 10, 5, 5]
             ];
-        
+
         testVector.forEach(function (t) {
             var min = t[0],
                 max = t[1],
@@ -39,13 +39,13 @@ describe("unit tests for the precision of axis", function() {
                 expectedValue = t[4];
 
             var precision = plot.computeValuePrecision(min, max, "x", ticks, tickDecimals);
-                
+
             expect(precision).toEqual(expectedValue);
         });
     });
-    
+
     it('should use the maximum precision when tickDecimals not specified', function() {
-        
+
         plot = $.plot("#placeholder", [sampledata], {});
 
         var testVector = [
@@ -53,21 +53,22 @@ describe("unit tests for the precision of axis", function() {
             [1, 1.01, 10, 3],
             [1, 1.1, 5, 2],
             [0.99963, 0.99964, null, 6],
-            [1, 1.00000000000001, 10, 16]
+            [1, 1.00000000000001, 10, 16],
+            [-200000, 200000, undefined, 0]
             ];
-        
+
         testVector.forEach(function (t) {
             var min = t[0],
                 max = t[1],
                 ticks = t[2],
                 expectedValue = t[3];
-              
+
             var precision = plot.computeValuePrecision(min, max, "x", ticks);
-                
+
             expect(precision).toEqual(expectedValue);
         });
     });
-    
+
     it('should increase precision for endpoints', function() {
         var testVector = [
             [1, 10, 10, 'linear', '1.00', '10.00'],
@@ -79,11 +80,13 @@ describe("unit tests for the precision of axis", function() {
             [1, 1.00000000001, 100, 'linear', '1.00000000000000', '1.00000000001000'],
             [1, 10, 10, 'log', '1.000', '10.00'],
             [0.1, 100, 11, 'log', '0.1000', '100.0'],
-            [0.99963, 0.99964, null, 'log', '0.99963000', '0.99964000']
+            [0.99963, 0.99964, null, 'log', '0.99963000', '0.99964000'],
+            [1, 2000000, null, 'log', '1.00', '2e6' ],
+            [-2000000, 2000000, null, 'linear', '-2000000', '2000000' ]
             ];
-        
+
         testVector.forEach(function (t) {
-            
+
             plot = $.plot("#placeholder", [sampledata], {
                 xaxes: [{
                     min: t[0],
@@ -94,11 +97,11 @@ describe("unit tests for the precision of axis", function() {
                     mode: t[3]
                 }]
             });
-            
+
             var minExpectedValue = t[4],
                 maxExpectedValue = t[5],
                 xaxis = plot.getAxes().xaxis;
-            
+
             expect(xaxis.ticks[0].label).toEqual(minExpectedValue);
             expect(xaxis.ticks[xaxis.ticks.length-1].label).toEqual(maxExpectedValue);
 
