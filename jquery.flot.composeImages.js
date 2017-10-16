eb5cdad0ba49539924dda49adfd88c179e0925f4
VirgilChindris <Virgil.Chindris@ni.com>
2017-10-16 15:53:45 +0300

    Renamed some parameters and added an image to page.

--- jquery.flot.composeImages.js
+++ jquery.flot.composeImages.js
@@ -1,6 +1,207 @@
+(function($){
+    "use strict";
 
-function composeImages(a, b) {
-    return a + b;
-}
+    function copyCanvasToImg(canvas, img) {
+        img.src = canvas.toDataURL('image/png');
+    }
 
-window.composeImages = composeImages;
+    function getCSSRules(document) {
+        var styleSheets = document.styleSheets,
+            rulesList = [];
+        for (var i = 0; i < styleSheets.length; i++) {
+            // in Chrome the external CSS files are empty when the page is loaded from directly disk
+            var rules = styleSheets[i].cssRules || [];
+            for (var j = 0; j < rules.length; j++) {
+                var rule = rules[j];
+                rulesList.push(rule.cssText);
+            }
+        }
+        return rulesList;
+    }
+
+    function embedCSSRulesInSVG(rules, svg) {
+        var text = [
+            '<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">',
+            //'<svg xmlns="http://www.w3.org/2000/svg">', //doesn't work in Firefox :(
+            '<style>',
+            '/* <![CDATA[ */',
+            rules.join('\n'),
+            '/* ]]> */',
+            '</style>',
+            svg.innerHTML,
+            '</svg>'
+        ].join('\n');
+        return text;
+    }
+
+    function copySVGToImgMostBrowsers(svg, img) {
+        var rules = getCSSRules(document),
+            text = embedCSSRulesInSVG(rules, svg),
+            blob = new Blob([text], {type:"image/svg+xml;charset=utf-8"}),
+            domURL = self.URL || self.webkitURL || self,
+            url = domURL.createObjectURL(blob);
+        img.src = url;
+    }
+
+    function copySVGToImgSafari(svg, img) {
+        // *** https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+        //
+        // Opera 8.0+
+        var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+        // Firefox 1.0+
+        var isFirefox = typeof InstallTrigger !== 'undefined';
+
+        // Safari 3.0+ "[object HTMLElementConstructor]"
+        var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+        // Internet Explorer 6-11
+        var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+        // Edge 20+
+        var isEdge = !isIE && !!window.StyleMedia;
+
+        // Chrome 1+
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+        // Blink engine detection
+        var isBlink = (isChrome || isOpera) && !!window.CSS;
+        // ***
+
+        var rules = getCSSRules(document),
+            text = embedCSSRulesInSVG(rules, svg),
+            data = "data:image/svg+xml;base64," + btoa(text);
+        img.src = data;
+    }
+
+    function copySVGToImg(svg, img) {
+        if (isEdge || isIE || isFirefox || isOpera || isChrome) {
+            copySVGToImgMostBrowsers(svg, img);
+        } else {
+            if (isSafari) {
+                copySVGToImgSafari(svg, img);
+            }
+        }
+    }
+
+
+    function prepareImagesToBeComposed(sources, destination) {
+        var Result = 0;
+        if (sources.length === 0) {
+            Result = -1; //nothing to do if called without sources
+        } else {
+            var minX = sources[0].genLeft;
+            var minY = sources[0].genTop;
+            var maxX = sources[0].genRight;
+            var maxY = sources[0].genBottom;
+
+            for (var i = 1; i < sources.length; i++) {
+                if (minX > sources[i].genLeft) {
+                    minX = sources[i].genLeft;
+                }
+
+                if (minY > sources[i].genTop) {
+                    minY = sources[i].genTop;
+                }
+            }
+
+            for (var i = 1; i < sources.length; i++) {
+                if (maxX < sources[i].genRight) {
+                    maxX = sources[i].genRight;
+                }
+
+                if (maxY < sources[i].genBottom) {
+                    maxY = sources[i].genBottom;
+                }
+            }
+
+            if ((maxX - minX <= 0) || (maxY - minY <= 0)) {
+                Result = -2;
+            } else {
+                destination.width = maxX - minX;
+                destination.height = maxY - minY;
+
+                for (var i = 0; i < sources.length; i++) {
+                    sources[i]["xCompOffset"] = sources[i].genLeft - minX;
+                    sources[i]["yCompOffset"] = sources[i].genTop - minY;
+                }
+            }
+        }
+        return Result;
+    }
+
+
+    function copyImgsToCanvas(sources, destination) {
+        var prepareImagesResult = prepareImagesToBeComposed(sources, destination);
+        if (prepareImagesResult === 0) {
+            var destinationCtx = destination.getContext('2d');
+
+            for (var i = 0; i < sources.length; i++) {
+                destinationCtx.drawImage(sources[i], sources[i].xCompOffset, sources[i].yCompOffset);
+            }
+        }
+        return prepareImagesResult;
+    }
+
+
+    function getSetDownloadableImgRef(downloadLinkElement, img) {
+        return function setDownloadableImgRef(result) {
+            downloadLinkElement.href = img.toDataURL('image/png');
+            return true;
+        }
+    }
+
+
+    function generateTempImageFromCanvasOrSvg(srcCanvasOrSvg, destImg) {
+        if (srcCanvasOrSvg.tagName === "CANVAS") {
+            copyCanvasToImg(srcCanvasOrSvg, destImg);
+        }
+
+        if (srcCanvasOrSvg.tagName === "svg") {
+            copySVGToImg(srcCanvasOrSvg, destImg);
+        }
+
+        //destImg will be used as temp image, so add to it some properties about position on page.
+        destImg["genLeft"] = srcCanvasOrSvg.getBoundingClientRect().left;
+        destImg["genTop"] = srcCanvasOrSvg.getBoundingClientRect().top;
+        destImg["genRight"] = srcCanvasOrSvg.getBoundingClientRect().right;
+        destImg["genBottom"] = srcCanvasOrSvg.getBoundingClientRect().bottom;
+    }
+
+
+    function getExecuteImgComposition(tempImgs, destination) {
+        return function executeImgComposition(result) {
+          var compositionResult = copyImgsToCanvas(tempImgs, destination);
+          return compositionResult;
+      };
+    }
+
+    function failureCallback(error) {
+      return -100; //a negative number
+    }
+
+
+    function getGenerateTempImg(tempImg, canvasOrSvgSource) {
+        return function doGenerateTempImg(successCallbackFunc, failureCallbackFunc) {
+            tempImg.onload = successCallbackFunc;
+            generateTempImageFromCanvasOrSvg(canvasOrSvgSource, tempImg);
+        };
+    }
+
+
+    function composeImages(canvasOrSvgSources, destinationImage) {
+        var tempImgs = [];
+        var allImgCompositionPromises = [];
+        for (var i = 0; i < canvasOrSvgSources.length; i++) {
+            var currentTempImg = new Image;
+            tempImgs.push(currentTempImg);
+            var currentPromise = new Promise(getGenerateTempImg(currentTempImg, canvasOrSvgSources[i]));
+            allImgCompositionPromises.push(currentPromise);
+        }
+        var lastPromise = Promise.all(allImgCompositionPromises).then(getExecuteImgComposition(tempImgs, destinationImage), failureCallback);
+        return lastPromise;
+    }
+
+
+    $.plot.composeImages = composeImages;
+})(jQuery);
--- tests/jquery.flot.composeImages.Test.js
+++ tests/jquery.flot.composeImages.Test.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 /* global $, describe, it, xit, xdescribe, after, afterEach, expect*/
 
-describe("flatdata plugin", function() {
-    var placeholder, plot;
+describe("composeImages", function() {
+    var sources, plot;
     var options = {
         series: {
             shadowSize: 0, // don't draw shadows
@@ -10,24 +10,75 @@ describe("flatdata plugin", function() {
             points: { show: true, fill: false, symbol: 'circle' }
         }
     };
+    var composeImages = $.plot.composeImages;
 
     beforeEach(function() {
-        placeholder = setFixtures('<div id="test-container" style="width: 600px;height: 400px">')
-            .find('#test-container');
+        sources = setFixtures(`<div id="test-container" style="width: 600px;height: 400px">
+        <svg id="svgSource" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="100" height="100" title="svg">
+          <circle id="c1" cx="10" cy="10" r="5" style="fill:red"/>
+          <circle id="c2" cx="30" cy="40" r="7" style="fill:#00FF00"/>
+          <circle id="c3" cx="50" cy="70" r="9" style="fill:blue"/>
+        </svg>
+        <svg id="svgSource2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="100" height="100" title="svg2">
+          <circle id="c1" cx="10" cy="10" r="5" style="fill:red"/>
+          <circle id="c2" cx="30" cy="40" r="7" style="fill:#00FF00"/>
+          <circle id="c3" cx="50" cy="70" r="9" style="fill:blue"/>
+        </svg>
+        </div>
+        <canvas id="myCanvas" width="300" height="150" style="border:1px solid #d3d3d3;"></canvas>
+        <img id="dest">
+        `).find('svg').toArray();
     });
 
-    it('should add two numbers', function () {
-        var result = composeImages(2, 3);
-        expect(result).toBe(5);
-    });
 
     it('should call async', function (done) {
-        var sources = [originalCanvas, originalSVG];
-        composeAsync(sources, finalDestinationCanvas, function() {
-            expect();
+        //var sources = [originalCanvas, originalSVG];
+        //var sources = [];
+        //var destinationImage = new Image;
+        var destinationImage = document.getElementById("myCanvas");
+
+        var ctx = destinationImage.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(99, 99);
+        ctx.moveTo(30, 60);
+        ctx.lineTo(70, 40);
+        ctx.stroke();
+/*
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.fillStyle = "red";
+        ctx.fillRect(0, 0, 50, 50);
+        function copy() {
+            var imgData = ctx.getImageData(0, 0, 1, 1);
+            var aData = imgData.data;
+            alert(aData);
+            ctx.putImageData(imgData, 10, 70);
+        }
+*/
+        composeImages(sources, destinationImage).then(function() {
+            /*
+            var canvas = document.createElement('canvas');
+            canvas.width = destinationImage.width;
+            canvas.height = destinationImage.height;
+            canvas.getContext('2d').drawImage(destinationImage, 0, 0, destinationImage.width, destinationImage.height);
+
+            //myCanvas
+*/
+            //var pixelData = canvas.getContext('2d').getImageData(51, 67, 1, 1).data;
+            var pixelData = destinationImage.getContext('2d').getImageData(51, 67, 1, 1).data;
+/*
+            copy();
+            var pixelData = c.getContext('2d').getImageData(30, 30, 1, 1).data;
+*/
+            //expect(pixelData[0]).toBe(0);
+            //expect(pixelData[1]).toBe(0);
+            //expect(pixelData[2]).toBe(255);
+            //expect(pixelData[3]).toBe(255);
             done();
-        });
+        }, null);
     });
+
 
     function composeAsync(sources, finalDestinationCanvas, onDone) {
         var originalCanvas = sources[0],
