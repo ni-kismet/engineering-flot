e92f76666817ede3d4a7cb48fbade728f351d9bc
VirgilChindris <Virgil.Chindris@ni.com>
2017-10-18 11:08:17 +0300

    Removed pic.html and style.css from branch, as they should not get to master. Did some cleanup to make the code look ok. Also, removed unused code.

--- jquery.flot.composeImages.js
+++ jquery.flot.composeImages.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
     "use strict";
 
     function copyCanvasToImg(canvas, img) {
@@ -36,7 +36,7 @@
     function copySVGToImgMostBrowsers(svg, img) {
         var rules = getCSSRules(document),
             text = embedCSSRulesInSVG(rules, svg),
-            blob = new Blob([text], {type:"image/svg+xml;charset=utf-8"}),
+            blob = new Blob([text], {type: "image/svg+xml;charset=utf-8"}),
             domURL = self.URL || self.webkitURL || self,
             url = domURL.createObjectURL(blob);
         img.src = url;
@@ -51,38 +51,15 @@
 
     function copySVGToImg(svg, img) {
         // *** https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-        //
-        // Opera 8.0+
-        var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-        // Firefox 1.0+
-        var isFirefox = typeof InstallTrigger !== 'undefined';
-
         // Safari 3.0+ "[object HTMLElementConstructor]"
         var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
-        // Internet Explorer 6-11
-        var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-        // Edge 20+
-        var isEdge = !isIE && !!window.StyleMedia;
-
-        // Chrome 1+
-        var isChrome = !!window.chrome && !!window.chrome.webstore;
-
-        // Blink engine detection
-        var isBlink = (isChrome || isOpera) && !!window.CSS;
-        // ***
-
-        if (isEdge || isIE || isFirefox || isOpera || isChrome) {
-            copySVGToImgMostBrowsers(svg, img);
+        if (isSafari) {
+            copySVGToImgSafari(svg, img);
         } else {
-            if (isSafari) {
-                copySVGToImgSafari(svg, img);
-            }
+            copySVGToImgMostBrowsers(svg, img);
         }
     }
-
 
     function prepareImagesToBeComposed(sources, destination) {
         var Result = 0;
@@ -93,8 +70,9 @@
             var minY = sources[0].genTop;
             var maxX = sources[0].genRight;
             var maxY = sources[0].genBottom;
+            var i = 0;
 
-            for (var i = 1; i < sources.length; i++) {
+            for (i = 1; i < sources.length; i++) {
                 if (minX > sources[i].genLeft) {
                     minX = sources[i].genLeft;
                 }
@@ -104,7 +82,7 @@
                 }
             }
 
-            for (var i = 1; i < sources.length; i++) {
+            for (i = 1; i < sources.length; i++) {
                 if (maxX < sources[i].genRight) {
                     maxX = sources[i].genRight;
                 }
@@ -120,7 +98,7 @@
                 destination.width = maxX - minX;
                 destination.height = maxY - minY;
 
-                for (var i = 0; i < sources.length; i++) {
+                for (i = 0; i < sources.length; i++) {
                     sources[i]["xCompOffset"] = sources[i].genLeft - minX;
                     sources[i]["yCompOffset"] = sources[i].genTop - minY;
                 }
@@ -128,7 +106,6 @@
         }
         return Result;
     }
-
 
     function copyImgsToCanvas(sources, destination) {
         var prepareImagesResult = prepareImagesToBeComposed(sources, destination);
@@ -141,15 +118,6 @@
         }
         return prepareImagesResult;
     }
-
-
-    function getSetDownloadableImgRef(downloadLinkElement, img) {
-        return function setDownloadableImgRef(result) {
-            downloadLinkElement.href = img.toDataURL('image/png');
-            return true;
-        }
-    }
-
 
     function generateTempImageFromCanvasOrSvg(srcCanvasOrSvg, destImg) {
         if (srcCanvasOrSvg.tagName === "CANVAS") {
@@ -167,18 +135,17 @@
         destImg["genBottom"] = srcCanvasOrSvg.getBoundingClientRect().bottom;
     }
 
-
     function getExecuteImgComposition(tempImgs, destinationCanvas) {
         return function executeImgComposition(result) {
-          var compositionResult = copyImgsToCanvas(tempImgs, destinationCanvas);
-          return compositionResult;
-      };
+            var compositionResult = copyImgsToCanvas(tempImgs, destinationCanvas);
+            return compositionResult;
+        };
     }
 
     function failureCallback(error) {
-      return -100; //a negative number
+        error = 0;
+        return -100; //a negative number
     }
-
 
     function getGenerateTempImg(tempImg, canvasOrSvgSource) {
         return function doGenerateTempImg(successCallbackFunc, failureCallbackFunc) {
@@ -187,12 +154,11 @@
         };
     }
 
-
     function composeImages(canvasOrSvgSources, destinationCanvas) {
         var tempImgs = [];
         var allImgCompositionPromises = [];
         for (var i = 0; i < canvasOrSvgSources.length; i++) {
-            var currentTempImg = new Image;
+            var currentTempImg = new Image();
             tempImgs.push(currentTempImg);
             var currentPromise = new Promise(getGenerateTempImg(currentTempImg, canvasOrSvgSources[i]));
             allImgCompositionPromises.push(currentPromise);
@@ -200,7 +166,6 @@
         var lastPromise = Promise.all(allImgCompositionPromises).then(getExecuteImgComposition(tempImgs, destinationCanvas), failureCallback);
         return lastPromise;
     }
-
 
     $.plot.composeImages = composeImages;
 })(jQuery);
