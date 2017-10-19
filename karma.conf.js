c5463c62dd65728747b54ac07c25bc1cfb31ab0a
Cristi Ingineru <cristi.ingineru@gmail.com>
2017-10-19 15:55:10 +0300

    Getting rid of PhantomJS and start using Edge and Safari (#154)
    * Getting rid of PhantomJS and start using Edge and Safari
    
    * Update the changelog

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,17 @@ var module;
 module.exports = function(config) {
     'use strict';
 
+    var browsersMatrix = {
+            'win': ['Edge', 'Firefox', 'Chrome'],
+            'linux': ['Firefox', 'Chrome'],
+            'mac': ['Safari', 'Firefox', 'Chrome']
+        },
+        isWin = /^win/.test(process.platform),
+        isLinux = /^linux/.test(process.platform),
+        isMac = /^darwin/.test(process.platform),
+        currentOSType = isWin ? 'win' : (isLinux ? 'linux' : 'mac'),
+        currentOSBrowsers = browsersMatrix[currentOSType];
+
     var coverage_sources = [
         'jquery.canvaswrapper.js',
         'jquery.colorhelpers.js',
@@ -43,7 +54,6 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: sources.concat([
-            'node_modules/phantomjs-polyfill-find/find-polyfill.js',
             'node_modules/webcharts-development-settings/testsUtils/utils/*.js',
             'tests/*.Test.js'
         ]),
@@ -59,7 +69,7 @@ module.exports = function(config) {
         },
 
         eslint: {
-            stopOnError: true,
+            stopOnError: config.stopOnEsLintError ? true : false,
             showWarnings: true,
             engine: {
                 configFile: 'node_modules/webcharts-development-settings/.eslintrc.json',
@@ -93,7 +103,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS', 'Firefox', 'Chrome'],
+        browsers: currentOSBrowsers,
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
@@ -115,7 +125,7 @@ module.exports = function(config) {
 
         settings.reporters.push('coverage');
         settings.reporters.push('coveralls');
-        settings.browsers = ['PhantomJS'];
+        settings.browsers = ['Chrome'];
     }
 
     config.set(settings);
