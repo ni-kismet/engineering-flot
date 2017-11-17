d79eed628d35a00083a74ffab5e5050465ed938a
octavianCraciun <ionut.octavian0409@gmail.com>
2017-11-17 15:39:43 +0200

    Fix snapshot for legend

--- dist/es5/jquery.flot.js
+++ dist/es5/jquery.flot.js
@@ -7207,18 +7207,44 @@ The plugin allso adds the following methods to the plot object:
     }
 
     function copySVGToImgMostBrowsers(svg, img) {
+        //get svg source.
         var rules = getCSSRules(document),
-            text = embedCSSRulesInSVG(rules, svg),
-            blob = new Blob([text], {type: "image/svg+xml;charset=utf-8"}),
+            source = embedCSSRulesInSVG(rules, svg);
+
+        //add name spaces.
+        if (!source.match(/^<svg[^>]+xmlns="http:\/\/www\.w3\.org\/2000\/svg"/)) {
+            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+        }
+        if (!source.match(/^<svg[^>]+"http:\/\/www\.w3\.org\/1999\/xlink"/)) {
+            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+        }
+
+        //add xml declaration
+        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+        var blob = new Blob([source], {type: "image/svg+xml;charset=utf-8"}),
             domURL = self.URL || self.webkitURL || self,
             url = domURL.createObjectURL(blob);
         img.src = url;
     }
 
     function copySVGToImgSafari(svg, img) {
+        //get svg source.
         var rules = getCSSRules(document),
-            text = embedCSSRulesInSVG(rules, svg),
-            data = "data:image/svg+xml;base64," + btoa(text);
+            source = embedCSSRulesInSVG(rules, svg),
+            data;
+
+        //add name spaces.
+        if (!source.match(/^<svg[^>]+xmlns="http:\/\/www\.w3\.org\/2000\/svg"/)) {
+            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+        }
+        if (!source.match(/^<svg[^>]+"http:\/\/www\.w3\.org\/1999\/xlink"/)) {
+            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+        }
+
+        //add xml declaration
+        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+        data = "data:image/svg+xml;base64," + btoa(source);
         img.src = data;
     }
 
