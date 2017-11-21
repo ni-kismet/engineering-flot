1bbd6ad252251bbc328d0e6feeccca2a8386588d
Raluca Portase <raluca.portase@ni.com>
2017-11-21 16:45:55 +0200

    update changelog

--- tests/jquery.flot.hover.Test.js
+++ tests/jquery.flot.hover.Test.js
@@ -35,7 +35,7 @@ describe("flot hover plugin", function () {
         var eventHolder = plot.getEventHolder(),
             axisx = plot.getXAxes()[0],
             axisy = plot.getYAxes()[0],
-            coords = [{x: axisx.p2c(0.5), y: axisy.p2c(0)}],
+            coords = [{x: axisx.p2c(0.5), y: axisy.p2c(-3.5)}],
             spy = jasmine.createSpy('spy');
 
         $(plot.getPlaceholder()).on('plothover', spy);
