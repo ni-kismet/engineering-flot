8e37a267279177a7da763aa604c2616de38c6e0c
atmgrifter00 <atm.grifter@gmail.com>
2018-07-09 12:09:40 -0500

    Getting rid of commented out lines.

--- jquery.flot.navigate.js
+++ jquery.flot.navigate.js
@@ -344,11 +344,6 @@ can set the default in the options.
                 plot.addEventHandler("dragstart", onDragStart, eventHolder, 0);
                 plot.addEventHandler("drag", onDrag, eventHolder, 0);
                 plot.addEventHandler("dragend", onDragEnd, eventHolder, 0);
-                // eventHolder.bind("dragstart", {
-                //     distance: 10
-                // }, onDragStart);
-                // eventHolder.bind("drag", onDrag);
-                // eventHolder.bind("dragend", onDragEnd);
                 eventHolder.bind("mousedown", onMouseDown);
                 eventHolder.bind("mouseup", onMouseUp);
             }
