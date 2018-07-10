be4cb9b07f2480769d5584cd1626f10a321290eb
Jonathan Meyer <atm.grifter@gmail.com>
2018-07-10 09:59:23 -0500

    Add event priority mechanism (#247)
    * Adding a mechanism to help control priority of event handlers.
    
    * Getting rid of commented out lines.
    
    * Creating a more proper key for the event manager.
    
    * Updates per code review request.
    
    * Code clean up.

--- source/jquery.flot.navigate.js
+++ source/jquery.flot.navigate.js
@@ -341,11 +341,9 @@ can set the default in the options.
             }
 
             if (o.pan.interactive) {
-                eventHolder.bind("dragstart", {
-                    distance: 10
-                }, onDragStart);
-                eventHolder.bind("drag", onDrag);
-                eventHolder.bind("dragend", onDragEnd);
+                plot.addEventHandler("dragstart", onDragStart, eventHolder, 0);
+                plot.addEventHandler("drag", onDrag, eventHolder, 0);
+                plot.addEventHandler("dragend", onDragEnd, eventHolder, 0);
                 eventHolder.bind("mousedown", onMouseDown);
                 eventHolder.bind("mouseup", onMouseUp);
             }
